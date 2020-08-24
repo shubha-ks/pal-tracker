aa7dbed0ec4a9dda4629784e291383d614edfb01
Prashant Sarvaiya <sarvaiya.prashant@gmail.com>
2020-08-24 10:40:07 +0000

    externalized configuration changes

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,36 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var message = Configuration.GetValue<string>("WELCOME_MESSAGE");
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ApplicationException("WELCOME_MESSAGE not configured.");
+            }
+            services.AddSingleton(sp => new WelcomeMessage(message));
+
+            var cfInstanceIndex = Configuration.GetValue<string>("CF_INSTANCE_INDEX");
+            if (string.IsNullOrEmpty(cfInstanceIndex))
+            {
+                throw new ApplicationException("Cf Instance Index not configured.");
+            }
+            var port = Configuration.GetValue<string>("PORT");
+            if (string.IsNullOrEmpty(port))
+            {
+                throw new ApplicationException("Port not configured.");
+            }
+            var memoryLimit = Configuration.GetValue<string>("MEMORY_LIMIT");
+            if (string.IsNullOrEmpty(memoryLimit))
+            {
+                throw new ApplicationException("Memory Limit not configured.");
+            }
+            var cfInstanceAddr = Configuration.GetValue<string>("CF_INSTANCE_ADDR");
+            if (string.IsNullOrEmpty(cfInstanceAddr))
+            {
+                throw new ApplicationException("Cf Instance Addr not configured.");
+            }
+            services.AddSingleton(sp => new CloudFoundryInfo(port, memoryLimit, cfInstanceIndex, cfInstanceAddr));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- test/PalTrackerTests/IntegrationTestServer.cs
+++ test/PalTrackerTests/IntegrationTestServer.cs
@@ -6,7 +6,37 @@ namespace PalTrackerTests
 {
     public static class IntegrationTestServer
     {
-        public static TestServer Start() =>
-            new WebApplicationFactory<Program>().Server;
+        //public static TestServer Start() =>
+            //new WebApplicationFactory<Program>().Server;
+
+            public static TestServer Start()
+            {
+                if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("WELCOME_MESSAGE")))
+                {
+                    System.Environment.SetEnvironmentVariable("WELCOME_MESSAGE", "Default message Test");
+                }
+
+                if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("PORT")))
+                {
+                    System.Environment.SetEnvironmentVariable("PORT", "port-123");
+                }
+
+                if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("MEMORY_LIMIT")))
+                {
+                    System.Environment.SetEnvironmentVariable("MEMORY_LIMIT", "500GB");
+                }
+
+                if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("CF_INSTANCE_INDEX")))
+                {
+                    System.Environment.SetEnvironmentVariable("CF_INSTANCE_INDEX", "1");
+                }
+
+                if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("CF_INSTANCE_ADDR")))
+                {
+                    System.Environment.SetEnvironmentVariable("CF_INSTANCE_ADDR", "add1234");
+                }
+
+                return new WebApplicationFactory<Program>().Server;
+            }
     }
 }
